f597f402aa4eacbbb5f142192540f6024801cfb6
Gleb Shkut <gleb.shkut@gmail.com>
2022-02-09 14:19:59 -0800

    created 1_queries

---
+++ 1_queries/5_All_My_Reservations.sql
@@ -0,0 +1,10 @@
+SELECT properties.* , reservations.*, AVG(property_reviews.rating)
+FROM reservations
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON reservations.id = property_reviews.reservation_id
+WHERE reservations.guest_id = 1 AND
+reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
+
